24133c9f711345494e3869dd2c245724db78d8d2
jtekrulez@gmail.com <jtekrulez@gmail.com@1452979a-8a71-11dd-887c-ed04d4280016>
2008-09-24 19:55:21 +0000

    Initial import.

---
+++ trunk/ bogglesolverhwk2 --username jtekrulez/Boggle Solver/src/Word.java
@@ -0,0 +1,44 @@
+import java.util.LinkedList;
+
+/**
+ * Word
+ * For CS3134 F08 HW2
+ *
+ * This class will represent a Word under construction.  Hint: Use a
+ * LinkedList of Strings to represent the word.  (Each string will be
+ * 1 letter long, of course.)
+ */
+public class Word {
+
+    /**
+     * Constructor.
+     */
+    public Word() {
+	// ...
+    }
+
+    /**
+     * Add a letter to the Word.
+     */
+    public void addLetter (String letter) {
+	// ...
+    }
+
+    /**
+     * "Flatten" the word back into a String.
+     */
+    public String toString() {
+	// ...
+    }
+
+    /**
+     * Make a copy of the word.  This is important for getting your
+     * recursion to work correctly (pass by value instead of by
+     * reference).
+     */
+    public Word makeCopy() {
+	// ...
+    }
+
+}
+
